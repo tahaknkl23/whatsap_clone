46ceae1057722dd8bc313a09374c967fa1a37c57
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-06-06 11:06:08 +0300

    chore: Add READ_CONTACTS and WRITE_CONTACTS permissions to AndroidManifest.xml and implement contact retrieval in SelectContactRepository

--- lib/features/select_contacts/repository/select_contact_repository.dart
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -1,8 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final SelectContactRepositoryProvider = Provider(
+  (ref) => SelectContactRepository(
+    firestore: FirebaseFirestore.instance,
+  ),
+);
+
+//burda şu işlemi yapıyoruz ki uygulama açıldığında kullanıcıdan izin istesin ve rehberi getirsin
 class SelectContactRepository {
-  final FirebaseFirestore firebaseFirestore;
+  final FirebaseFirestore firestore;
   SelectContactRepository({
-    required this.firebaseFirestore,
+    required this.firestore,
   });
+
+//burda rehberi getirme işlemini yapıyoruz
+  Future<List<Contact>> getContacts() async {
+    List<Contact> contacts = [];
+    try {
+      if (await FlutterContacts.requestPermission()) {
+        await FlutterContacts.getContacts(
+          withProperties: true,
+        );
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return contacts;
+  }
 }
